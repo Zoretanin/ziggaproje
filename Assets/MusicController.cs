a52637a3cc4a0a3f597b5fd867420cffef1e16bc
Zoretanin <hellbourne35@hotmail.com>
2025-11-03 16:35:13 +0300

    Update MusicController.cs

--- Assets/MusicController.cs
+++ Assets/MusicController.cs
@@ -1,18 +1,51 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MusicController : MonoBehaviour
 {
     public AudioSource musicSource;
+    public Text buttonText;
+
+    void Start()
+    {
+        // Daha önce ayar kaydedilmiþ mi diye kontrol et
+        if (PlayerPrefs.GetInt("MusicEnabled", 1) == 1)
+        {
+            musicSource.Play();
+            buttonText.text = "Mute Music";
+        }
+        else
+        {
+            musicSource.Pause();
+            buttonText.text = "Play Music";
+        }
+    }
 
     public void ToggleMusic()
     {
         if (musicSource.isPlaying)
         {
             musicSource.Pause();
+            buttonText.text = "Play Music";
+            PlayerPrefs.SetInt("MusicEnabled", 0); // Kapalý kaydet
         }
         else
         {
             musicSource.Play();
+            buttonText.text = "Mute Music";
+            PlayerPrefs.SetInt("MusicEnabled", 1); // Açýk kaydet
         }
     }
+
+
+    void Awake()
+    {
+        DontDestroyOnLoad(buttonText);
+    }
+
+
+
+
 }
+    
+
